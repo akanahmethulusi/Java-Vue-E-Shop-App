becb29d8c0a98b3438f501fff8d505b962bb3a75
muazahmet <akanahmethulusi@gmail.com>
2026-01-07 10:33:19 +0100

    Initial commit

---
+++ src/main/java/at/codefabrik/Java_Vue_E_Shop_App/JavaVueEShopAppApplication.java
@@ -0,0 +1,13 @@
+package at.codefabrik.Java_Vue_E_Shop_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaVueEShopAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaVueEShopAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/codefabrik/Java_Vue_E_Shop_App/JavaVueEShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package at.codefabrik.Java_Vue_E_Shop_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaVueEShopAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
